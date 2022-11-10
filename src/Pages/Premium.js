ad3598f7b363cd215b37d090508e07a84b853e59
MRJ2812 <mdjoy2812@gmail.com>
2022-11-10 23:14:06 +0600

    commit-13

--- src/Pages/Premium.js
+++ src/Pages/Premium.js
@@ -22,28 +22,3 @@ const Premium = () => {
 
 export default Premium;
 
-/*
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { useLoaderData } from 'react-router-dom';
-
-const Premium = () => {
-    const detail = useLoaderData();
-
-    return (
-        <div className='text-center'>
-            <h1 style={{ color: 'gold' }}>Premium Content</h1>
-            <Card className='mb-4' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={detail.image} />
-                <Card.Body>
-                    <Card.Title>{detail.CourseTitle}</Card.Title>
-                </Card.Body>
-            </Card>
-        </div>
-    );
-};
-
-export default Premium;
-
-
-*/
